a7c902fc5ab89d256c9f601557d15f80633b652b
Sean Blake <88250197+seanB888@users.noreply.github.com>
2021-08-01 00:47:27 -0400

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by SEAN BLAKE on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
