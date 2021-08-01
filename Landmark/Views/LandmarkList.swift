86f376183ca687fa879c585e9bfc6ac2e5828834
Sean Blake <88250197+seanB888@users.noreply.github.com>
2021-08-01 12:19:17 -0400

    Listing all the data

---
+++ Landmark/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmark
+//
+//  Created by SEAN BLAKE on 8/1/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
