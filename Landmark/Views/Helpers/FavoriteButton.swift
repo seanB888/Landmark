50e36e56fb6a96157dcd4c13f97b0509cb10f4bb
Sean Blake <88250197+seanB888@users.noreply.github.com>
2021-08-02 00:40:26 -0400

    Add Favorite to the LandmarkDetail view

---
+++ Landmark/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmark
+//
+//  Created by SEAN BLAKE on 8/2/21.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }) {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? Color.yellow : Color.gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
